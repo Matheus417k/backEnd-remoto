03a1f258f50aedf96a19540786025076be0681eb
MATHEUS DE ALMEIDA LUIZ <matheus.luiz7@senaisp.edu.br>
2026-06-16 12:31:14 -0300

    atualizar

--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
 import 'dotenv/config';
 import express from 'express';
 import routes from './routes/routes.js';
-import cors from 'cors';
+import { initializeDatabase } from './configs/Database.js';
+
 
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:5173'
-}));
-app.use('/uploads', express.static('uploads'));
+
 app.use(express.json());
 app.use('/', routes);
 
-app.listen(process.env.SERVER_PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${process.env.SERVER_PORT}`);
+
+initializeDatabase().then(() => {
+    app.listen(process.env.SERVER_PORT, () => {
+        console.log(`Servidor rodando na porta ${process.env.SERVER_PORT}`);
+    });
+}).catch(err => {
+    console.error("Erro ao inicializar o banco de dados:", err);
 });
