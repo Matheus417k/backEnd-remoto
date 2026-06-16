bdcecfcd6a5e582f4b195b910bd1c594b067908b
MATHEUS DE ALMEIDA LUIZ <matheus.luiz7@senaisp.edu.br>
2026-06-16 16:04:19 -0300

    atualiza7

--- src/server.js
+++ src/server.js
@@ -3,13 +3,13 @@ import express from 'express';
 import routes from './routes/routes.js';
 import { initializeDatabase } from './configs/Database.js';
 
+
 const app = express();
 
+
 app.use(express.json());
-
-app.use('/uploads/images', express.static('src/uploads/images'));
-
 app.use('/', routes);
+
 
 initializeDatabase().then(() => {
     app.listen(process.env.SERVER_PORT, () => {
